0d72c61f5aea05bec8f31478e5db3b5fe56e1ccc
Joshua Gustafson and Jake Steinberg <silversevenfox@yahoo.com>
2016-02-17 09:22:32 -0800

    add test

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -37,7 +37,15 @@ public class Task {
   mCompleted = true;
   }
 
-  public static ArrayList<Task> all() {
+  public static ArrayList<Task>  all() {
     return instances;
   }
+
+  public static Task find(int id) {
+   try {
+     return instances.get(id - 1);
+   } catch (IndexOutOfBoundsException e) {
+     return null;
+   }
+ }
 }
