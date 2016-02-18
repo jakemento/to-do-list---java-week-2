6b5c1f470638ece42b46c3abd3e078be33c81571
Jake Steinberg and Joshua Gustafson <jakesteinberg@gmail.com>
2016-02-18 13:32:34 -0800

    add a new class, category

---
+++ src/main/java/Category.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Category {
+  private static ArrayList<Category> instances = new ArrayList<Category>();
+
+  private String mName;
+  private int mId;
+  private ArrayList<Task> mTasks;
+
+
+  public Category(String name) {
+    mName = name;
+    instances.add(this);
+    mId = instances.size();
+    mTasks = new ArrayList<Task>();
+  }
+
+  public String getName() {
+    return mName;
+  }
+
+  public ArrayList<Task> getTasks() {
+    return mTasks;
+  }
+
+
+  public void addTask(Task task) {
+      mTasks.add(task);
+    }
+
+  public int getId() {
+   return mId;
+  }
+
+
+
+  public static void clear() {
+    instances.clear();
+  }
+
+
+  public static ArrayList<Category> all() {
+    return instances;
+  }
+
+  public static Category find(int id) {
+   try {
+     return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException e) {
+     return null;
+    }
+  }
+
+
+}
---
+++ src/test/java/ClearRule.java
@@ -0,0 +1,4 @@
+protected void after() {
+  Task.clear();
+  Category.clear();
+}
